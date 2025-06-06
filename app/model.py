92bcd5dc522da2403158ffa33d2a9378c91d437e
Matheus-Deve <168038782+Matheus-Deve@users.noreply.github.com>
2025-06-06 17:20:50 -0300

    create consultas,py

--- app/model.py
+++ app/model.py
@@ -4,3 +4,9 @@ from models.tabelas import *
 #Importa os cadastros das tabelas do BD
 from models.cadastros import *
 
+def consultaGeralManifestacao():
+    manifestacoesQuery = Manifestacao.query.all()
+
+    manifestacoesDic = [manifestacao.trans_dic() for manifestacao in manifestacoesQuery]
+
+    return manifestacoesDic
--- app/models/conexao.py
+++ app/models/conexao.py
@@ -7,6 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 #Importa o os para a ultilização segura de caminhos de arquivos (usando caminhos relativos dinâmicos)
 import os
 
+#Importa a conexão com banco de dados
+from models.conexao import *
+
 diretorio = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(diretorio, 'database', 'reclamaif.db')
 
