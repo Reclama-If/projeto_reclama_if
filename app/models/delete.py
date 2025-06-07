d035b4c8fd8fbdfe2a08eb892532d104e5d5176a
Sandstorm-23 <samuelrocha10.com@gmail.com>
2025-06-07 18:44:28 -0300

    organização de arquivos

---
+++ app/models/delete.py
@@ -0,0 +1,7 @@
+#Importação do sqlalchemy para as funções ligadas ao BD
+from flask_sqlalchemy import SQLAlchemy
+
+#Importa a conexão com banco de dados
+from models.conexao import *
+
+from models.tabelas import *
